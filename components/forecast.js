e726b7bed80829316be0150d7a81e9fbaebfa70f
Brian Neeland <brian.neeland@protonmail.com>
2022-04-13 16:57:15 -0600

    Added column-based 14-day data summaries

--- components/forecast.js
+++ components/forecast.js
@@ -1,9 +1,9 @@
 function toCelcius(tempInF) {
-  return ((tempInF - 32) * 5/9).toFixed(1)
+  return ((tempInF - 32) * 5/9).toFixed(0)
 }
 
 function toKmph(speedInMph) {
-  return (speedInMph * 1.609344).toFixed(1)
+  return (speedInMph * 1.609344).toFixed(0)
 }
 
 export default function Forecast(props) {
@@ -13,6 +13,7 @@ export default function Forecast(props) {
 
   return (
     <>
+      {/* Current day - summary */}
       <div className="border flex">
         <div className="border flex-none">
           <div className="border flex justify-center items-center w-32 h-32">
@@ -31,11 +32,17 @@ export default function Forecast(props) {
           <div className="w-full">
             <div className="border flex">
               <div className="border flex-1">Precipitation</div>
-              <div className="border">{weatherData.currentConditions.precipprob && `${weatherData.currentConditions.precipprob}% probability, `}{weatherData.currentConditions.precip && `${weatherData.currentConditions.precip} `}{weatherData.currentConditions.preciptype && `${weatherData.currentConditions.preciptype}`}</div>
+              <div className="border">
+                {weatherData.currentConditions.precipprob ? `${weatherData.currentConditions.precipprob}% probability` : `—`}
+                {weatherData.currentConditions.preciptype && ` ${weatherData.currentConditions.preciptype}`}
+              </div>
             </div>
             <div className="border flex">
               <div className="border flex-1">Wind</div>
-              <div className="border">{toKmph(weatherData.currentConditions.windspeed)} km/h {weatherData.currentConditions.winddir}&deg;</div>
+              <div className="border">
+                {toKmph(weatherData.currentConditions.windspeed)} km/h
+                {weatherData.currentConditions.winddir && ` ${weatherData.currentConditions.winddir}°`}
+              </div>
             </div>
             <div className="border flex">
               <div className="border flex-1">Cloud cover</div>
@@ -47,22 +54,86 @@ export default function Forecast(props) {
             </div>
           </div>
         </div>
-
       </div>
 
-      <hr />
-
+      {/* Current day - detail */}
       <div>
-        {weatherData.days.map(day => (
-          <div key={day.datetime}>
-            <div>
-              <code>{day.datetime}: High: {((day.tempmax - 32) * 5/9).toFixed(1)}&nbsp;&deg;C / Low: {((day.tempmin - 32) * 5/9).toFixed(1)}&nbsp;&deg;C, POP: {day.precipprob}, {day.description}</code>
-            </div>
-            {day.hours.map(hour => (
-              <div key={hour.datetime}>
-                <code>{hour.datetime}: {((hour.temp - 32) * 5/9).toFixed(1)}&nbsp;&deg;C, POP: {hour.precipprob}, {hour.conditions}</code>
-              </div>
+        <div className="border">
+
+          <div>Current day's temperatures</div>
+
+          <div className="flex overflow-auto">
+            {weatherData.days[0].hours.map(hour => (
+              <div>{hour.datetime}<br />
+              {toCelcius(hour.temp)} &deg;C</div>
             ))}
+          </div>
+
+        </div>
+
+        <div className="border">
+
+          <div>Current day's precipitation</div>
+
+          <div className="flex overflow-auto">
+            {weatherData.days[0].hours.map(hour => (
+              <div>{hour.datetime}<br />
+              {hour.precipprob}%</div>
+            ))}
+          </div>
+
+        </div>
+
+        <div className="border">
+
+          <div>Current day's windspeed</div>
+
+          <div className="flex overflow-auto">
+            {weatherData.days[0].hours.map(hour => (
+              <div>{hour.datetime}<br />
+              {toKmph(hour.windspeed)} km/h</div>
+            ))}
+          </div>
+
+        </div>
+      </div>
+
+      {/* 14 days */}
+      <div className="flex overflow-auto">
+        {weatherData.days.map(day => (
+          <div className="grid grid-rows-">
+            <div>
+              {day.datetime}
+            </div>
+            <div>
+              {day.icon}<br />
+              {day.conditions}
+              {day.description}
+            </div>
+            <div>
+              High: {day.tempmax}<br />
+              Low: {day.tempmin}<br />
+              [Sparkline of temp] {/* day.hours.map(hour => ...) */}
+            </div>
+            <div>
+              {day.precip}%<br />
+              {day.preciptype}<br />
+              {day.precipcover}<br />
+              [Sparkline of precip] {/* day.hours.map(hour => ...) */}
+            </div>
+            <div>
+              {toKmph(day.windspeed)} km/h<br />
+              {day.winddir}&deg;<br />
+              [Sparkline of wind] {/* day.hours.map(hour => ...) */}
+            </div>
+            <div>
+              {day.cloudcover}%<br />
+              [Sparkline of cloud] {/* day.hours.map(hour => ...) */}
+            </div>
+            <div>
+              {day.humidity}%<br />
+              [Sparkline of humid] {/* day.hours.map(hour => ...) */}
+            </div>
           </div>
         ))}
       </div>
